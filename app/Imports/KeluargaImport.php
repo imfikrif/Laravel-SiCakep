24f28683dfa79ba2fd9af6d7a9b321229387b5f3
fbesar19 <fbesar.id@gmail.com>
2021-09-19 23:24:19 +0700

    [Update] Revisi import data keluarga

--- app/Imports/KeluargaImport.php
+++ app/Imports/KeluargaImport.php
@@ -5,8 +5,9 @@ namespace App\Imports;
 use App\Models\Keluarga;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class KeluargaImport implements ToModel
+class KeluargaImport implements ToModel, WithStartRow
 {
     /**
     * @param array $row
@@ -19,5 +20,10 @@ class KeluargaImport implements ToModel
             'nomor'             => $row[0],
             'kepala_keluarga'   => $row[1],
         ]);
+    }
+
+    public function startRow(): int
+    {
+        return 2;
     }
 }
